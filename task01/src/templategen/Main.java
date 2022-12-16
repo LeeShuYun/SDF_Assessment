074b9f25fa97b7af6a30a32fbd6d9a00172a1d30
LeeShuyun <quaternion.design@gmail.com>
2022-12-16 16:13:52 +0800

    final version, please mark this one

--- task01/src/templategen/Main.java
+++ task01/src/templategen/Main.java
@@ -16,93 +16,91 @@ import java.util.Map;
  */
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         // instructions
-        System.out.println("Usage>>> javac -cp classes templategen.Main <csv> <template>");
-        
-        //parse commands       
+        // javac -cp classes templategen.Main <csv> <template>
+
+        // parse commands
         String csvFile = args[0];
         String templateFile = args[1];
 
-        // Processing CSV =====================================================================================
+        // Processing CSV
+        // =====================================================================================
         // create a List of Hashmap with the format {String what to replace:string actual value}
         // eg. {first_name: Harry}
 
-        //prep work opening csv file and loading text
-        String csvfilePath = csvFile;
+        // prep work opening csv file and loading text
         FileReader fileReader = new FileReader(csvFile);
         BufferedReader br = new BufferedReader(fileReader);
-        
-        //grabbing header first
-        String header = br.readLine(); 
-        // System.out.println("HEADER>>>" + header);
+
+        // grabbing header first
+        String header = br.readLine();
         String[] headerLabels = header.trim().split(",");
+
+        //the hashmap we're putting our {first_name: Harry} KV into
         HashMap<String, String> csvMap;
-        //contains our replacement name variables
+
+        // contains our replacement name variables
         List<HashMap<String, String>> csvMapList = new LinkedList<>();
         String csvLine;
         String[] csvLineList;
-        //creating the list of hashmap of replacements to put into the template
-        while ((csvLine = br.readLine()) != null){
+        // creating the list of hashmap of replacements to put into the template
+        while ((csvLine = br.readLine()) != null) {
             csvMap = new HashMap<>();
-            //read a line
-            System.out.println("CSVLINE:" + csvLine); //test string
+            // read a line
             csvLineList = csvLine.trim().split(",");
-            
-            //matching each label with its corresponding value and inserting into hashmap
+
+            // matching each label with its corresponding value and inserting into hashmap
             for (Integer i = 0; i < headerLabels.length; i++) {
                 csvMap.put(headerLabels[i], csvLineList[i]);
-                System.out.println( headerLabels[i] + ": " + csvLineList[i]);
             }
-            //pop the finished map of replacement into the list for  later processing
+            // pop the finished map of replacement into the list for later processing
             csvMapList.add(csvMap);
         }
 
-        //now that we read all the data, we close stream
+        // now that we read all the data, we close stream
         br.close();
         fileReader.close();
 
-        //processing the template ====================================================
-        //first we read the template file
+        // processing the template ====================================================
+        // first we read the template file
         String templFilePath = templateFile;
         File tempFileObj = Paths.get(templFilePath).toFile();
         FileReader tempFileReader = new FileReader(tempFileObj);
         BufferedReader bufferedreader = new BufferedReader(tempFileReader);
 
-        //prep work
+        // prep work
         String templateOutput;
-        String completedTemplate ="";
-        //generating the complete template String first
-        while ((templateOutput = bufferedreader.readLine()) != null){
-            // System.out.println(templateOutput);
-            // String line = bufferedreader.readLine();
+        String completedTemplate = "";
+
+        // generating the complete template String first
+        while ((templateOutput = bufferedreader.readLine()) != null) {
             completedTemplate = completedTemplate + templateOutput;
             completedTemplate += "\n";
         }
         bufferedreader.close();
         tempFileReader.close();
-        
-        //searching thru the completed template string to find words to replace
+
+        // searching thru the completed template string to find words to replace
         String finalMail = completedTemplate;
-        for (HashMap<String, String> replacementWordsMap : csvMapList) {//for each set of replacement, we process and print
-            // System.out.println("hashmap first names: " + replacementWordsMap.get("first_name"));
-            for (String replacementWord : headerLabels) {  //for each replacement word we look through the String
-                //check if the list has <<replacement>>
-                // if (completedTemplate.contains("<<" + replacementWord + ">>")){
-                    // remove the <<>> and replace the variable with the correct one value
-                    finalMail = finalMail.replace("<<" + replacementWord + ">>", replacementWordsMap.get(replacementWord));
-                    
-                    // System.out.println("<<" + replacementWord + ">>" + " => " + replacementWordsMap.get(replacementWord));
-                    // }
-                } 
-                finalMail = finalMail.replace("\\n", "\n");
-                //now that the set of replacements is complete, we print it out
-                System.out.println(finalMail);
-                
-                //reset the current instance of mail for the next mail
-                finalMail = completedTemplate;
-                
+        // for each set of replacement, we process and print
+        for (HashMap<String, String> replacementWordsMap : csvMapList) {
+            // for each replacement word we look through the single template String
+            for (String replacementWord : headerLabels) { 
+                // remove the <<>> and replace the variable with the correct one value
+                finalMail = finalMail.replace("<<" + replacementWord + ">>", replacementWordsMap.get(replacementWord));
+
             }
-                
-}
+            //replacing the escaped \n character with line break
+            finalMail = finalMail.replace("\\n", "\n");
+
+            // now that all replacements is complete for one line of csv, we print it out
+            System.out.println(finalMail);
+
+            // reset the current instance of mail for the next mail
+            finalMail = completedTemplate;
+
+        }
+
+    }
 }
